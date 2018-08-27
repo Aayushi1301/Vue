9bfa096995b06d4da84358a5246448ee235e2600
Aayushi1301 <aayushi@zemosolabs.com>
2018-08-27 23:51:31 +0530

    Vue js frontend

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<!-- It binds the DOM with the Vue instance data-->
+
+<template>
+  <div id="app">
+    <google-map></google-map>
+
+
+  </div>
+</template>
+
+<!-- Loading the MyMap component -->
+<script>
+import Vue from 'vue';
+import MyMap from './components/MyMap'
+Vue.component('google-map',MyMap)
+
+</script>
+
---
+++ src/components/Heatmap.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <!-- Initializing the props of vue-google-heatmap -->
+  <vue-google-heatmap :points="points"
+                      :height="600"
+                      :width="1275"
+                      :initialZoom="2"
+                      :lat="48.0196"
+                      :lng="66.9237"
+                       />
+</template>
+
+<script>
+  //importing the packages
+  import Vue from 'vue';
+  import VueGoogleHeatmap from 'vue-google-heatmap';
+
+  Vue.use(VueGoogleHeatmap, {
+    apiKey:"AIzaSyCdsCZwgVbKBnvwqtPQ8Mqcv5P0lNzuxs8"
+  });
+
+  //declaring the component which can be reused later
+  export default{
+    props:['points']
+  }
+</script>
